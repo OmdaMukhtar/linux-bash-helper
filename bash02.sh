d98eac2384711948eb9525d6364239fa87baa2b9
Omda Mukhtar <abuoop123@gmail.com>
2019-09-03 20:04:27 +0200

    send password as argument to sudo

--- bash02.sh
+++ bash02.sh
@@ -1,9 +1,13 @@
 
 #!/bin/bash
-cd 
-cd Desktop/atroha2/atroha/
-code .
 
-gnome-terminal --tab --title="server" --command="bash -c 'cd /opt/lampp;sudo -S $target_user; ./lampp start; $SHELL'" --tab --title="npm" --command="bash -c 'cd; cd Desktop/atroha2/atroha;npm run watch; $SHELL'"
+cd # out from current path to root
 
-php artisan serve
+cd Desktop/folder1/folder2/ # enter to path of my project
+
+code .  # open my project in vscode
+
+gnome-terminal --tab --title="server" --command="bash -c 'cd /opt/lampp;echo your_password|sudo -S ./lampp start; $SHELL'" --tab --title="npm" --command="bash -c 'cd; cd Desktop/folder1/folder2;npm run watch; $SHELL'"
+
+php artisan serve # run server
+
