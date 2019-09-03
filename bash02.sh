7e588840b7021a550d1db819f11e62098a87c394
Omda Mukhtar <abuoop123@gmail.com>
2019-09-03 18:44:55 +0200

    make bash working correctly with my machin but it make me enter password and I dont like so.

--- bash02.sh
+++ bash02.sh
@@ -1,17 +1,9 @@
 
 #!/bin/bash
-#cd 
-#cd Desktop/atroha2/atroha/
-#code .
+cd 
+cd Desktop/atroha2/atroha/
+code .
 
-#php artisan serve
-#xdotool key ctrl+shift+t  php artisan serve
-
-
-#gnome-terminal & cd /etc/lampp & sudo ./lampp start
-
-
-
-#read -p "Password: " -s szPassword
-target_user="omda"
 gnome-terminal --tab --title="server" --command="bash -c 'cd /opt/lampp;sudo -S $target_user; ./lampp start; $SHELL'" --tab --title="npm" --command="bash -c 'cd; cd Desktop/atroha2/atroha;npm run watch; $SHELL'"
+
+php artisan serve
