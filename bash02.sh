5ac98c7b1cfb13a8b80d9ba1f059e7d5535c7ce0
Omda Mukhtar <abuoop123@gmail.com>
2019-10-17 11:25:04 +0200

    fix error to open vscode

--- bash02.sh
+++ bash02.sh
@@ -9,14 +9,16 @@ then
 	cd ;
 	exit 1;
 else	
+
 	cd # out from current path to root
 
 	cd Desktop/atroha2/atroha/ # enter to path of my project
+
+	code .  # open my project in vscode
 
 	gnome-terminal	--tab --title="server" --command="bash -c 'cd /opt/lampp;echo $pass|sudo -S ./lampp start; $SHELL'" --tab --title="npm" --command="bash -c 'cd; cd Desktop/atroha2/atroha;npm run watch; $SHELL'"
 
 	php artisan serve # run server
 
-	code .  # open my project in vscode
 fi
 
